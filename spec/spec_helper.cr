71074458cad444c87fa470a181e5e6867c1c567d
Iain McNulty <iainmcnulty@gmail.com>
2018-09-02 21:16:19 +0100

    Correct root path expansion in spec_helper

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,5 +1,7 @@
 require "spec"
-ExecutablePath = File.expand_path("../bin/mysqldumpsplitter", File.dirname(__FILE__))
+Root = File.expand_path("..", File.dirname(__FILE__))
+
+ExecutablePath = File.expand_path("bin/mysqldumpsplitter", Root)
 def path_from_root(path)
   File.expand_path(path,  File.dirname(__FILE__))
 end
